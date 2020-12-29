ef5b8301631e4ab26c90ba5f279631d43485aded
Jack Lawson <ajacksified@gmail.com>
2020-12-29 10:31:55 -0500

    Use new ATR API

--- pages/api/activity.js
+++ pages/api/activity.js
@@ -1,9 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import qs from 'querystring';
-import axios from 'axios';
-import cheerio from 'cheerio';
+import { request } from 'axios';
 import { omit } from 'lodash';
-import { parseStringPromise } from 'xml2js';
 
 import Cors from 'cors';
 import initMiddleware from '../../lib/initMiddleware';
@@ -21,14 +19,15 @@ const cors = initMiddleware(
 // TODO fix LoC / LoS parsing
 
 const REGEXES = {
+  COMBAT_EVENT_PARTICIPATION: /Combat event participation added to Combat Record by the COO : .*id=(?<eventId>\d+).*">(?<eventString>.*)<\/a>/,
   MEDAL_AWARDED: /^Medal awarded : [^\(]+ \((?<medalShorthand>.+)\)/,
   MEDALS_AWARDED: /^Medals awarded : (?<qty>\d+) [^\(]+ \((?<medalShorthand>.+)s?\)/,
-  BATTLE_COMPLETED: /^Battle completed : (?<battleType>\S+) (?<battleId>\d+)/,
+  BATTLE_COMPLETED: /^Battle completed : .*id=(?<battleId>\d+).*">(?<battleType>.*)</,
   SUBMITTED_BATTLE_REVIEW: /^Submitted review for battle (?<battleType>\S+) (?<battleId>\d+)/,
   IU_COMPLETED: /^IU Course added to Academic Record by the SOO : \[(?<iuCourse>[^\(]+)]/,
   IU_REMOVED: /^IU Course removed from Academic Record by the SOO : \[(?<iuCourse>[^\(]+)]/,
   NEW_COMBAT_RATING: /^New Combat Rating achieved : (?<combatRating>.*)/,
-  NEW_COMPETITION: /^Submitted competition approved : ID# (?<competitionId>\d+)/,
+  NEW_COMPETITION: /^Submitted competition approved : .*id=(?<competitionId>\d+).*">/,
   NEW_REPORT: /^Submitted a new (?<reportType>.*) report/,
   NEW_FCHG: /^New Fleet Commander's Honor Guard rank achieved : (?<fchg>\S+)/,
   NEW_COOP_RATING: /New COOP\/PVE Rating achieved : (?<rating>\S+)/,
@@ -43,6 +42,8 @@ const REGEXES = {
   UPDATED_INPR: /^Updated Imperial Navy Personnel Record \(INPR\)/,
   UPDATED_ROSTER: /^Updated the roster information for (?<unit>.+)/,
   RANK_SET_BY_TCCOM: /^New rank set by the TCCOM : .+ \((?<rankShorthand>.+)\)/,
+
+  IGNORE_COMBAT_EVENT_REMOVED: /^Combat event participation removed/,
 };
 
 function activityTypeByString(activityString) {
@@ -62,55 +63,31 @@ function activityTypeByString(activityString) {
   };
 }
 
-async function loadActivityData(pilotId, startDate, endDate) {
-  const { data: html } = await axios.get(`https://tc.emperorshammer.org/record.php?pin=${pilotId}&type=atr`);
+async function loadActivityData(pilotId, startDate, endDate, page, pageSize) {
+  const url = `https://api.emperorshammer.org/atr/${pilotId}`;
 
-  const $ = cheerio.load(html);
+  const params = {
+    ...(page ? { page } : {}),
+    ...(pageSize ? { pageSize } : {}),
+    ...(startDate ? { startDate } : {}),
+    ...(endDate ? { endDate } : {}),
+  };
 
-  let activityRows = $('body > center > table > tbody > tr > td:nth-child(2) > p:nth-child(6) > table:nth-child(2) > tbody > tr > td > table > tbody > tr > td:nth-child(1) > table:nth-child(4) tr');
+  const { data: activity } = await request({ url, params });
 
-  // Sometimes the page is different. I dunno
-  if (!activityRows.length) {
-    activityRows = $('body > center > table > tbody > tr > td:nth-child(2) > p:nth-child(6) > table:nth-child(2) > tbody > tr > td > table > tbody > tr > td:nth-child(1) > table:nth-child(2) tr');
+  if (!activity || !activity.activity) {
+    return [];
   }
 
-  const activityData = [];
-
-  const data = $(activityRows).each((index, row) => {
-    const cells = $(row).children();
-    const dateString = $(cells.get(1)).text();
-    const activityString = $(cells.get(2)).text();
-
-    if (!dateString || !activityString) {
-      return;
+  const activityData = activity.activity.map(({ date, activity }) => ({
+    activity: {
+      ...activityTypeByString(activity),
+      date: (new Date(date * 1000)),
     }
-
-    const date = new Date(dateString);
-
-    if (isNaN(date)) {
-      return;
-    }
-
-    date.setHours(-1 * date.getUTCHours(), 0, 0, 0);
-
-    if (startDate && startDate > date) {
-      return;
-    }
-
-    // enddate is 2020-10-17
-    // date is 2020-10-17
-    if (endDate && endDate < date) {
-      return;
-    }
-
-    activityData.push({
-      date: date.toISOString(),
-      ...activityTypeByString(activityString),
-    });
-  });
+  }));
 
   // Accumulate by type
-  const activityDataByType = activityData.reduce((acc, activity) => ({
+  const activityDataByType = activityData.reduce((acc, { activity }) => ({
     ...acc,
     [activity.type]: [
       ...(acc[activity.type] || []),
@@ -151,7 +128,7 @@ async function loadActivityData(pilotId, startDate, endDate) {
 }
 
 async function formatActivityData(pilotId, activityData) {
-  const { data: pilotJSON } = await axios.get(`http://api.emperorshammer.org/pilot/${pilotId}`);
+  const { data: pilotJSON } = await request({ url: `http://api.emperorshammer.org/pilot/${pilotId}` });
   const title = `${pilotJSON.label} #${pilotJSON.PIN}`;
   const underline = "".padStart(title.length, "=");
 
@@ -163,7 +140,7 @@ async function formatActivityData(pilotId, activityData) {
     if (activityType === "MEDALS_AWARDED") {
       text.push(Object.keys(activityData[activityType]).map((medal) => `* ${medal} x ${activityData[activityType][medal]}`).join('\n') + '\n');
     } else {
-      text.push(activityData[activityType].map((a) => `* [${a.date}] ${a.activityString}`).join('\n') + '\n');
+      text.push(activityData[activityType].map((a) => `* [${a.date.toDateString()}] ${a.activityString}`).join('\n') + '\n');
     }
   }, []);
 
@@ -177,34 +154,14 @@ export default async (req, res) => {
 
   const {
     pilotId,
-    startDate: startDateString,
-    endDate: endDateString,
+    startDate,
+    endDate,
     format = "json",
+    page = 1,
+    pageSize = 50,
   } = qs.parse(url.split("?")[1]);
 
-  let startDate;
-  let endDate;
-
-  if (startDateString) {
-    startDate = new Date(startDateString);
-  }
-
-  if (endDateString) {
-    endDate = new Date(endDateString);
-  }
-
-  if ((startDate && isNaN(startDate)) || (endDate && isNaN(endDate))) {
-    res.statusCode = 422;
-
-    res.json({
-      error: "Invalid start date or end date supplied.",
-      params: { pilotId, startDateString, endDateString },
-    });
-
-    return;
-  }
-
-  const activityData = await loadActivityData(pilotId, startDate, endDate);
+  const activityData = await loadActivityData(pilotId, startDate, endDate, page, pageSize);
 
   res.statusCode = 200;
 
